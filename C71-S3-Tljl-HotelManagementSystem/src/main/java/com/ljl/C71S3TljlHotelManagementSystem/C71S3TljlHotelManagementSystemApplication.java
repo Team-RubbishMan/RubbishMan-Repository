457dbd8bfbded3885c75faef331d0af14ecfd4c9
Da <Da@DESKTOP-K0B51ON>
2020-02-15 13:39:11 +0800

    员工后台登录（未完成）

--- C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/C71S3TljlHotelManagementSystemApplication.java
+++ C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/C71S3TljlHotelManagementSystemApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class C71S3TljlHotelManagementSystemApplication {
+/*
+ * @MapperScan("com.ljl.C71S3TljlHotelManagementSystem")
+ */public class C71S3TljlHotelManagementSystemApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(C71S3TljlHotelManagementSystemApplication.class, args);
