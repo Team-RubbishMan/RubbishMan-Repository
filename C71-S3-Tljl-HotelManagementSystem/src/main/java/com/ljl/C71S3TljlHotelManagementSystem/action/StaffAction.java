048f5507a4d85364cf610343b4ea156a83088636
LiuZiyuan-CS <56821599+LiuZiyuan-CS@users.noreply.github.com>
2020-02-16 13:20:34 +0800

    Update StaffAction.java

--- C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/StaffAction.java
+++ C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/StaffAction.java
@@ -20,11 +20,4 @@ public class StaffAction {
 	public String signup() {
 		return "/back/signup";
 	}
-	//
-	@PostMapping("/back/dologin")
-	public ModelAndView dologin(@RequestParam("email")String name,@RequestParam("password")String password) {
-		System.out.println("用户名："+name+"====");
-		System.out.println("密码"+password+"----");
-		return new ModelAndView("redirect:/back/frame.html");
-	}
 }
