720f7e558b9787823cb0d1f9e99f564b3d27d4c7
刘子源 <@LAPTOP-AM9AT35N>
2020-02-29 20:14:42 +0800

    1

--- C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/UserAction.java
+++ C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/UserAction.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserAction {
+	/**
+	 * 打开，登录，注册，忘记密码页面
+	 * @author 刘子源
+	 * @return
+	 */
 	@GetMapping("front/signIn.html")
 	public String SignIn() {
 		return "front/signIn";
