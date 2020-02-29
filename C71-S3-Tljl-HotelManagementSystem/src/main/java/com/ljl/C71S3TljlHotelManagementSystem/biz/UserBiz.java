dda739d9f614c5ce867bea7ead606c92e4323437
刘子源 <@LAPTOP-AM9AT35N>
2020-02-29 20:13:14 +0800

    bootbot.js，网页修改，userbiz建立

---
+++ C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/biz/UserBiz.java
@@ -0,0 +1,10 @@
+package com.ljl.C71S3TljlHotelManagementSystem.biz;
+
+import javax.annotation.Resource;
+
+import com.ljl.C71S3TljlHotelManagementSystem.dao.UserMapper;
+
+public class UserBiz {
+	@Resource
+	private UserMapper userMapper;
+}
