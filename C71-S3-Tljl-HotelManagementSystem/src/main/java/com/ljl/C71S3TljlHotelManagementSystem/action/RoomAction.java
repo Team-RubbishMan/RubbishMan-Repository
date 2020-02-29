8ae3cb0d9f6ca80aa13ef976b685e7c0a8d8e266
刘子源 <@LAPTOP-AM9AT35N>
2020-02-29 19:56:22 +0800

    1

--- C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/RoomAction.java
+++ C71-S3-Tljl-HotelManagementSystem/src/main/java/com/ljl/C71S3TljlHotelManagementSystem/action/RoomAction.java
@@ -8,9 +8,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.github.pagehelper.PageInfo;
 import com.ljl.C71S3TljlHotelManagementSystem.bean.Room;
 import com.ljl.C71S3TljlHotelManagementSystem.bean.RoomType;
 import com.ljl.C71S3TljlHotelManagementSystem.biz.BizException;
@@ -37,11 +39,26 @@ public class RoomAction {
 		//model.addAttribute("lstRoomTypeInfo", lstRoomTypeInfo);
 		return "back/roommanagement";
 	}
+	
 	@GetMapping("back/roompricemanagement.html")
-	public String roomPriceManagement(Model model,@RequestParam(defaultValue = "1")String page) {
-		model.addAttribute("lstRoomTypeInfo", roomBiz.loadRoomTypeInfo(Integer.parseInt(page)));
+	public String roomPriceManagement(Model model) {
+		model.addAttribute("lstRoomTypeInfo", roomBiz.loadRoomTypeInfo());
 		return "back/roompricemanagement";
 	}
+	
+	/**
+	 * 加载表格数据
+	 * @author Da
+	 * @param limit 每页大小
+	 * @param pageNumber 当前页数
+	 * @return
+	 */
+	@ResponseBody
+    @RequestMapping("back/loadRoomTypeInfo")
+    public PageInfo<RoomType> getPage(@RequestParam(value = "limit", required = true) Integer limit,
+                                      @RequestParam(value = "pageNumber", required = true) Integer pageNumber){
+        return roomBiz.getPage(pageNumber,limit);
+    }
 	
 	@PostMapping("back/roomMaintenance")
 	public Result roomMaintenance(int id) {
@@ -59,13 +76,92 @@ public class RoomAction {
 		}
 	}
 	
-
+	/**
+	 * 房型删除
+	 * @author Da
+	 * @param id id
+	 * @return
+	 */
+	@PostMapping("back/roomTypeDelete")
+	@ResponseBody
+	public Result roomTypeDelete(Integer id) {
+		try {
+			roomBiz.deleteRoomTypeInfo(id);
+			return new Result(1,"操作成功",null);
+		} catch (BizException e) {
+			e.printStackTrace();
+			return new Result(0,e.getMessage(),null);
+		}
+	}
+	
+	/**
+	 * 房型删除
+	 * @author Da
+	 * @param id id数组
+	 * @return
+	 */
+	@PostMapping("back/roomTypeDeletes")
+	@ResponseBody
+	public Result roomTypeDelete(@RequestParam String[] id) {
+		try {
+			roomBiz.deleteRoomTypeInfo(id);
+			return new Result(1,"操作成功",null);
+		} catch (BizException e) {
+			e.printStackTrace();
+			return new Result(0,e.getMessage(),null);
+		}
+	}
+	
+	/**
+	 * 房型更新
+	 * @author Da
+	 * @param roomType
+	 * @return
+	 */
 	@PostMapping("back/roomTypeUpdate")
 	@ResponseBody
-	public Result roomTypeManagement(RoomType roomType) {
-		System.out.println(roomType);
+	public Result roomTypeUpdate(RoomType roomType) {
 		try {
 			roomBiz.updateRoomTypeInfo(roomType);
+			return new Result(1,"操作成功",null);
+		} catch (BizException e) {
+			e.printStackTrace();
+			return new Result(0,e.getMessage(),null);
+		}
+	}
+	
+	/**
+	 * 检查房型是否存在
+	 * @author Da
+	 * @param typeName
+	 * @return
+	 * @throws BizException 
+	 */
+	@PostMapping("back/typeNameIsUsed")
+	@ResponseBody
+	public Result doRoomType(String typeName) throws BizException {
+		try {
+			if(roomBiz.selectTypeName(typeName)) {
+				return new Result(1,"操作成功",null);
+			}
+		} catch (BizException e) {
+			e.printStackTrace();
+			return new Result(0,e.getMessage(),null);
+		}
+		return null;
+	}
+	
+	/**
+	 * 房型新增
+	 * @author Da
+	 * @param roomType
+	 * @return
+	 */
+	@PostMapping("back/addRoomType")
+	@ResponseBody
+	public Result addRoomType(RoomType roomType) {
+		try {
+			roomBiz.addRoomType(roomType);
 			return new Result(1,"操作成功",null);
 		} catch (BizException e) {
 			e.printStackTrace();
